43ab4faf725be5eae0c21b268bfb9138dff2c984
Marcos Turisco <turisco.tech@gmail.com>
2026-04-28 10:18:42 -0300

    feat: atualizando mensagem de health check da API

--- src/main/java/com/agenda/demo/rest/AgendaController.java
+++ src/main/java/com/agenda/demo/rest/AgendaController.java
@@ -9,7 +9,7 @@ public class AgendaController {
 
     @GetMapping("/api/status")
     public ResponseEntity<String> status() {
-        return ResponseEntity.ok("API da Agenda Atualizada via CD Automático!");
+        return ResponseEntity.ok("API da Agenda Atualizada 100% via CI/CD");
     }
 
 }
