8c87723b8abd5dd795c756cd7cc6c9134bf5963f
Marcos Turisco <turisco.tech@gmail.com>
2026-05-01 21:34:49 -0300

    refactor: remove Kafka manual beans para usar o Auto-Configuration do Spring

--- src/main/java/com/agenda/demo/infra/config/AgendaConfig.java
+++ src/main/java/com/agenda/demo/infra/config/AgendaConfig.java
@@ -10,37 +10,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 public class AgendaConfig {
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-
-        // 1. Onde o Kafka está rodando?
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-
-        // 2. Como serializar a Chave da mensagem? (Vamos usar String)
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        // 3. Como serializar o Valor da mensagem? (Nosso JSON já foi convertido para String pelo ObjectMapper)
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
-    }
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory() {
+//        Map<String, Object> configProps = new HashMap<>();
+//
+//        // 1. Onde o Kafka está rodando?
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//
+//        // 2. Como serializar a Chave da mensagem? (Vamos usar String)
+//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        // 3. Como serializar o Valor da mensagem? (Nosso JSON já foi convertido para String pelo ObjectMapper)
+//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        return new DefaultKafkaProducerFactory<>(configProps);
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate() {
+//        return new KafkaTemplate<>(producerFactory());
+//    }
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -68,4 +60,5 @@ public class AgendaConfig {
     public DeletarContatoUseCase deletarContatoUseCase(ContatoRepository repository) {
         return new DeletarContatoUseCase(repository);
     }
+
 }
